1bcb44f2ca7aadb57ba68acc73b5db23599428d4
Panghui-L <384762718@qq.com>
2016-06-03 21:46:14 +0800

    homework1-2

---
+++ CashCard.java
@@ -0,0 +1,26 @@
+package com.orilore.lxh.test;
+
+public class CashCard {
+
+	 String number;
+	 
+	 int balance;
+	 
+	 int bonus;
+	 
+	 CashCard(String name,int balance,int bonus){
+		 
+		 this.number = number;
+		 
+		 this.balance = balance;
+		 
+		 this.bonus = bonus;
+		 
+		 
+		 
+		 
+	 }
+	
+	
+	
+}
